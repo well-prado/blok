08a1289fd07d366287d4da53e30bfa9ec0907c06
Wellington Prado <well.cp@hotmail.com>
2026-06-28 15:17:58 -0300

    test(runner): guard tpl mapper compile caching

--- nodes/utility/helpers@1.0.0/src/llmAgent.ts
+++ nodes/utility/helpers@1.0.0/src/llmAgent.ts
@@ -113,9 +113,10 @@ export default defineNode({
 				publish(channel: string, opts: { event?: string; data: unknown }): SsePublishedEvent;
 			};
 		}
+		const moduleName = "@blokjs/trigger-sse";
 		let bus: ReturnType<SseBusModule["_getSSEBus"]>;
 		try {
-			const mod = (await import("@blokjs/trigger-sse")) as SseBusModule;
+			const mod = (await import(moduleName)) as SseBusModule;
 			bus = mod._getSSEBus();
 		} catch (err) {
 			const msg = err instanceof Error ? err.message : String(err);
